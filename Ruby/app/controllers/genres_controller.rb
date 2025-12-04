cfae1de58b3d6c716e03a864ac465c49c1cf01ba
Eguaras Zoe <eguaraszoe@gmail.com>
2025-12-04 02:35:52 +0000

    feat: Scaffold de géneros con restricción de borrado

---
+++ Ruby/app/controllers/genres_controller.rb
@@ -0,0 +1,72 @@
+class GenresController < ApplicationController
+  before_action :set_genre, only: %i[ show edit update destroy ]
+
+  # GET /genres or /genres.json
+  def index
+    @genres = Genre.all
+  end
+
+  # GET /genres/1 or /genres/1.json
+  def show
+  end
+
+  # GET /genres/new
+  def new
+    @genre = Genre.new
+  end
+
+  # GET /genres/1/edit
+  def edit
+  end
+
+  # POST /genres or /genres.json
+  def create
+    @genre = Genre.new(genre_params)
+
+    respond_to do |format|
+      if @genre.save
+        format.html { redirect_to @genre, notice: "Género creado correctamente." }
+        format.json { render :show, status: :created, location: @genre }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @genre.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /genres/1 or /genres/1.json
+  def update
+    respond_to do |format|
+      if @genre.update(genre_params)
+        format.html { redirect_to @genre, notice: "Género actualizado correctamente.", status: :see_other }
+        format.json { render :show, status: :ok, location: @genre }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @genre.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /genres/1 or /genres/1.json
+  def destroy
+    respond_to do |format|
+      begin
+        @genre.destroy!
+        format.html { redirect_to genres_path, notice: "Género eliminado correctamente.", status: :see_other }
+      rescue ActiveRecord::DeleteRestrictionError
+        format.html { redirect_to genres_path, alert: "No se puede eliminar este género porque tiene productos asociados." }
+      end
+    end
+  end  
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_genre
+      @genre = Genre.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def genre_params
+      params.expect(genre: [ :name ])
+    end
+end
--- Ruby/db/schema.rb
+++ Ruby/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_04_022303) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_04_023221) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_04_022303) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_genres_on_name", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
